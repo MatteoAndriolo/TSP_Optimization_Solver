0f7fdc28e865794c5212ffdc623420f46b20d443
Andriolo Matteo <matteo.andriolo.2@studenti.unipd.it>
2023-03-16 16:28:57 +0100

    added documentation 2opt

--- include/refinement.h
+++ include/refinement.h
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include "logger.h"
 
+/**
+ * Applies the 2-opt algorithm to a given distance matrix and node list (in place).
+ * 
+ * @param matrix A pointer to the distance matrix.
+ * @param size_node The number of nodes in the tour.
+ * @param node A pointer to the array of nodes in the tour.
+ *
+ * @return void
+ */
 void two_opt(double *matrix, int size_node, int *path);
 
 #endif
