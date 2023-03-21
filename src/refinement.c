5fee125ff180c1a48cb2e1c06d2228ef56dd6a87
Reevoc <pierma1997@gmail.com>
2023-03-21 18:30:41 +0100

    check two_opt if works now

--- src/refinement.c
+++ src/refinement.c
@@ -6,9 +6,13 @@ void two_opt(double *distance_matrix, int nnode, int *path, double *tour_lenght)
     double improvement = -INFINITY;
     int index_min = -1;
     double min_cost = -1;
-    while (improvement < -300)
+    double count = 0;
+    /*- count è un contatore che ha come logica quella di fare  check se non ci sono stati un tot di improvement in 2 optù
+      - ho aggiunto nella condizione if il fatto che se improvement è >0 non deve fare nulla e aumenta il count 
+      - se alla fine della fiera non trova abbastanza improvement taglia il while e finisce 2opt*/
+    while (count < ((int)nnode * 0.4))
     {
-        // 3 4 6 7 9 10 1
+        count = 0;
         for (int i = 0; i < nnode; i++)
         {
             index_min = i + 1;
@@ -28,13 +32,18 @@ void two_opt(double *distance_matrix, int nnode, int *path, double *tour_lenght)
                     DEBUG_COMMENT("refinement:two_opt", "index_min: %d", index_min);
                 }
             }
-
+            improvement =   distance_matrix[path[i] * nnode + path[index_min]]                // ADD THE SMALL ONE FOUND
+                          - distance_matrix[path[i] * nnode + path[i + 1]]                  // ELIMINATE OLD ONE
+                          + distance_matrix[path[i + 1] * nnode + path[index_min + 1]]      // ADD LINK BETWEEN i+1 and min_index + 1
+                          - distance_matrix[path[index_min] * nnode + path[index_min + 1]]; // ELIMINATE index_min and the position after min_index
+            // ADD THE BASELINE WITH THE MIN FOUND SUCCESSOR
+            *tour_lenght += improvement;
+            CRITICAL_COMMENT("refinement:two_opt", "Improvement: %f", improvement);
             CRITICAL_COMMENT("refinement:two_opt", "add i + min_index: %f", min_cost);
             CRITICAL_COMMENT("refinement:two_opt", "delated min_index and min_index + 1: %f", distance_matrix[path[index_min] * nnode + path[index_min + 1]]);
             CRITICAL_COMMENT("refinement:two_opt", "delated i and i + 1 : %f", distance_matrix[path[i] * nnode + path[i + 1]]);
             CRITICAL_COMMENT("refinement:two_opt", "add i + 1 and min_index + 1: %f", distance_matrix[path[i + 1] * nnode + path[index_min + 1]]);
-
-            if (index_min != i + 1) // found new optimal edge (i, index_min)
+            if (index_min != i + 1 && improvement < 0) // found new optimal edge (i, index_min)
             {
                 int ti = i + 1;
                 for (int z = 0; z < (int)(index_min - i + 1) / 2; z++) // reverse order cells (i+1,index_min)
@@ -43,14 +52,7 @@ void two_opt(double *distance_matrix, int nnode, int *path, double *tour_lenght)
                     path[z + ti] = path[index_min - z];
                     path[index_min - z] = temp;
                 }
-            }
-            improvement = distance_matrix[path[i] * nnode + path[index_min]]                // ADD THE SMALL ONE FOUND
-                          - distance_matrix[path[i] * nnode + path[i + 1]]                  // ELIMINATE OLD ONE
-                          + distance_matrix[path[i + 1] * nnode + path[index_min + 1]]      // ADD LINK BETWEEN i+1 and min_index + 1
-                          - distance_matrix[path[index_min] * nnode + path[index_min + 1]]; // ELIMINATE index_min and the position after min_index
-            // ADD THE BASELINE WITH THE MIN FOUND SUCCESSOR
-            *tour_lenght += improvement;
-            CRITICAL_COMMENT("refinement:two_opt", "Improvement: %f", improvement);
+            }else count++;
         }
     }
 }
