d40477d9389ef4efd1e0e51a7d95f4376caa02c8
Piermarco Giustini <61749102+Reevoc@users.noreply.github.com>
2023-05-02 15:11:15 +0200

    simulate anealling works ?

--- src/metaheuristic.c
+++ src/metaheuristic.c
@@ -9,10 +9,7 @@ double randomBetween_d(double lowerBound, double upperBound)
 
 double energy_probabilities(double cost_current, double cost_new, double T, double coefficient)
 {
-    // the coefficient shold be between 0 and 1
-    // the more is near to 0 the more we have probability to take bad moves 
-    // the more is near to 1 the more we are not allowd to take a bad move 
-    // suggestion between 0.6 and 0.9 but there is a proble does not depend on the current value new cost and current cost
+    //double delta = (cost_new - cost_current) / (cost_current + cost_new) * coefficient;
     if (cost_new < cost_current) return 1;
     return exp( - coefficient / T);
 }
@@ -25,21 +22,22 @@ void simulate_anealling(const double *distance_matrix, int *path, int nnodes, do
     int *current_state = malloc(nnodes * sizeof(int));
     double current_state_cost = *tour_length;
     memcpy(current_state, path, nnodes * sizeof(int));
-
+    double old_coefficient_cost = 0;
     int *new_state = malloc(nnodes * sizeof(int));
     double new_state_cost;
 
     for (int k = 0; k < k_max; k++)
     {
         T = (1 - ((double)k / k_max));
-        kick_function(distance_matrix, current_state, nnodes, &current_state_cost, 5);
+        old_coefficient_cost = current_state_cost;
+        kick_function(distance_matrix, current_state, nnodes, &current_state_cost, 4);
         two_opt(distance_matrix, nnodes, current_state, &current_state_cost, INFINITY);
         new_state_cost = current_state_cost;
         memcpy(new_state, current_state, nnodes * sizeof(int));
-        double value = energy_probabilities(current_state_cost, new_state_cost, T, 0.6);
+        double value = energy_probabilities(old_coefficient_cost, new_state_cost, T, 0.8);
         double random = randomBetween_d(0, 1);
         DEBUG_COMMENT("metaheuristic.c:simulate_anealling", "k: %d, T: %f, value: %f, random: %f", k, T, value, random);
-        if (value >= random)
+        if (value > random)
         {
             memcpy(current_state, new_state, nnodes * sizeof(int));
             current_state_cost = new_state_cost;
